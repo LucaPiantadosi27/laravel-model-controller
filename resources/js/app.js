ac6873c5b508d789899f47511275b89756831ca3
LucaPiantadosi27 <145672456+LucaPiantadosi27@users.noreply.github.com>
2024-04-09 12:03:35 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+// Import our custom CSS
+import '~resources/scss/app.scss';
+
+
+// Precompilazione immagini
+import.meta.glob([
+    '../img/**'
+    ]);
---
+++ resources/views/Layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Titolo</title>
+
+    <!-- Includiamo gli assets con la direttiva @ vite -->
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
