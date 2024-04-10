bd05b3834eac83bb4cef7d08ebd68dd070f81794
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-04-10 18:13:56 +0200

    Esercizio completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,45 @@
 @section ('content')
 
 
+
+<div class="container py-5  ">
+   <h1> Lista Film: </h1> 
+    <div class="row row-cols-4 ">
+
+        @foreach($movies as $movie)
+
+            <div class="col p-2">
+                <div class="card border-black rounded-3" style="width: 17rem">
+                    
+                    <div class="card-body bg-body-tertiary ">
+
+                      <h4 class="card-title">{{$movie->title}}</h4>
+                      <p class="card-text">({{$movie->nationality}})</p>
+                      <p class="card-text">({{$movie->original_title}})</p>
+                      <a href="#" class="btn btn-primary">Go Watch</a>
+
+                    </div>
+                </div>
+                
+
+            </div>
+
+        @endforeach
+
+    </div>
+
+
+
+
+
+
+    
+    
+    
+
+   
+
+    
+
+</div>
 @endsection
